7e802ffa6d760ef678d4910174591e655f86d82e
Ivanov <ivanovtimofey1@gmail.com>
2025-07-14 21:52:06 +0200

    Implement Basic Number Theory Functions - Implement greatest common denominator function - Implment least common multiple function

---
+++ include/numbers/number_theory/basic.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <numbers/number_types/num_aliases.hpp>
+
+namespace Numbers
+{
+    IntT gcd(IntT lhs, IntT rhs);
+
+    IntT lcm(const IntT lhs, const IntT rhs);
+} // namespace Numbers
---
+++ src/number_theory/basic.cpp
@@ -0,0 +1,32 @@
+#include <numbers/number_theory/basic.hpp>
+
+#include <cmath>
+
+namespace Numbers
+{
+
+    IntT gcd(IntT lhs, IntT rhs)
+    {
+        lhs = std::abs(lhs);
+        rhs = std::abs(rhs);
+        while (rhs != 0)
+        {
+            IntT tmp = lhs % rhs;
+            lhs = rhs;
+            rhs = tmp;
+        }
+
+        return lhs;
+    }
+
+    IntT lcm(const IntT lhs, const IntT rhs)
+    {
+        if (lhs == 0 || rhs == 0)
+        {
+            return 0;
+        }
+
+        return lhs * rhs / gcd(lhs, rhs);
+    }
+
+} // namespace Numbers
